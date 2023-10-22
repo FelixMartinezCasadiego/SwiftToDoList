c760c0fbbdc056b24fcc3825fa940ea8ab04987b
felix <felix.martinezcasadiego@gmail.com>
2023-10-22 12:40:39 +0200

    updated Login part

--- Views/LoginView.swift
+++ Views/LoginView.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                // Header
+                HeaderView()
+                
+                // LoginForm
+                Form {
+                    TextField("Email Address", text: $email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    SecureField("Password ", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    Button {
+                        //action
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(.blue)
+                            
+                            Text("Log In")
+                                .foregroundColor(.white)
+                                .bold()
+                        }
+                    }
+                }
+                
+                // Create Account
+                VStack {
+                    Text("New around here?")
+                    NavigationLink("Create an Account", destination: RegisterView())
+                }
+                .padding(.bottom, 50)
+                
+                Spacer()
+            }
+        }
     }
 }
 
