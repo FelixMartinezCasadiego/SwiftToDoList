e8579b91d1c2e73477b04811a5154e68e691d32b
felix <felix.martinezcasadiego@gmail.com>
2023-10-25 22:44:47 +0200

    delete func

--- Views/ToDoListView.swift
+++ Views/ToDoListView.swift
@@ -5,21 +5,34 @@
 //  Created by Felix Martinez Casadiego on 22/10/23.
 //
 
+import FirebaseFirestoreSwift
 import SwiftUI
 
 struct ToDoListView: View {
-    @StateObject var viewModel = ToDoListViewViewModel()
+    @StateObject var viewModel: ToDoListViewViewModel
+    @FirestoreQuery var items:[ToDoListItem]
     
     private let userId: String
     
     init(userId: String) {
         self.userId = userId
+        // users/<id>/todos/<entries>
+        self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos")
+        self._viewModel = StateObject(wrappedValue: ToDoListViewViewModel(userId: userId))
     }
     
     var body: some View {
         NavigationView {
             VStack {
-                
+                List(items) {
+                    item in ToDoListItemView(item: item)
+                        .swipeActions {
+                            Button("Delete") {
+                                viewModel.delete(id: item.id)
+                            }
+                            .tint(.red)
+                        }
+                }
             }
             .navigationTitle("To Do List")
             .toolbar {
@@ -37,5 +50,5 @@ struct ToDoListView: View {
 }
 
 #Preview {
-    ToDoListView(userId: "")
+    ToDoListView(userId: "EWJSTqrSMlZWch8iKvbSpe4hggf2")
 }
