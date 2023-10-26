37fbf7a1667f2570f1092ea77a607d4aa9f94c2d
felix <felix.martinezcasadiego@gmail.com>
2023-10-27 00:15:31 +0200

    profile view

--- ViewModels/ToDoListItemViewViewModel.swift
+++ ViewModels/ToDoListItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Felix Martinez Casadiego on 22/10/23.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 /// ViewModel for single to do list item view (each row in items list)
@@ -12,6 +14,18 @@ class ToDoListItemViewViewModel: ObservableObject {
     init() {}
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
     }
 }
